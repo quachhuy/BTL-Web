0cf273ebdf4339f9dbccd3c6c436603aacf1e1f3
maingocduy <2051063514@e.tlu.edu.vn>
2022-03-29 17:19:35 +0700

    duy up cua hang lan 1

---
+++ file-js(Cửa-hàng)/scrip.js
@@ -0,0 +1,21 @@
+function checkEmail(){
+    let email = $('#txtEmail').val()
+    let EmailRegex = /\S+@\S+.\S+/
+    if (EmailRegex.test(email)){
+        return true
+    }
+    return false
+}
+$("#btnRegister").click(function(e){
+    e.preventDefault();
+    if(checkEmail()){
+        $("#statusOfEmail").text('Email của bạn chuẩn xác.Vui lòng kiểm tra hòm thư của bạn')
+        $("#statusOfEmail").css('color','#00FFFF')
+        $(".status").css('background','#f1f1f1')
+
+    }else{
+        $("#statusOfEmail").text('Bạn cần điền đúng thông tin được yêu cầu')
+        $("#statusOfEmail").css('color','red')
+        $(".status").css('background','#f1f1f1')
+
+    }})
