58011419ed8776c244a8b7a2e7be36f05cb6ba24
maingocduy <2051063514@e.tlu.edu.vn>
2022-04-11 15:13:16 +0700

    them trang dang nhap

---
+++ src-login/js-login/login.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.eye-icon').click(function(){
+        $(this).toggleClass('appear')
+        $(this).children('i').toggleClass('fa-eye-slash fa-eye')
+        if ($(this).hasClass('appear')){
+            $(this).prev().attr('type','text')
+        }else{
+            $(this).prev().attr('type','password')
+        }
+    })
+    
+});
+$('.form-submit').click(function(e){
+    e.preventDefault()
+    var username = $("#username").val(),
+        password = $("#password").val();
+    if(username == '' || password == ''){
+        swal({
+            title: "Bạn chưa nhập tên đăng nhập hoặc mật khẩu!",
+            text: "Xin mời nhập lại",
+            icon: "error",
+            button: "OK",
+          });
+    }
+})
