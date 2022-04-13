b7db38a5affeb1b224a4af4c0f68ef7c0ad3d247
unknown <2051060555@e.tlu.edu.vn>
2022-04-14 00:30:39 +0700

    sua login cua trang admin

---
+++ Admin-reactjs/frontend/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import { Card, CardContent, CardHeader } from '@material-ui/core'
+const AdminPanel = () => (
+    <Card>
+        <CardHeader title='Welcome to Admin Panel' />
+        <CardContent>
+            <div>
+                <p>HAVE A NICE DAY</p>
+            </div>
+        </CardContent>
+    </Card>
+)
+export default AdminPanel
